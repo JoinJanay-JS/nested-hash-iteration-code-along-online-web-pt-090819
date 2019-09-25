51d42c4f424940add3f2cdf41d49dbb2a58a4d33
Janay Bailey <tjanay87@gmail.com>
2019-09-25 14:39:04 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,11 +15,11 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-contacts.each do |person, contact_detail_hash|
-  if person == "Freddy Mercury"
-  contact_detail_hash.each do |attribute, data|
-    if attribute == :favorite_ice_cream_flavors
-      data.delete_if {|ice cream| ice_cream == "strawberry"}
+  contacts.each do |person, contact_details_hash|
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
         end
       end
     end
